861b3086fd7cebf652e539eb80e1640af486f4c2
Aethor <oow.autre@gmail.com>
2022-06-01 20:11:28 +0200

    NeuralContextSelector inference

--- conivel/datas/context.py
+++ conivel/datas/context.py
@@ -113,7 +113,7 @@ class NeighborsContextSelector(ContextSelector):
 from typing import Literal, cast
 from dataclasses import dataclass
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, dataloader
 from transformers import BertForTokenClassification, BertForSequenceClassification, BertTokenizerFast, DataCollatorWithPadding  # type: ignore
 from transformers.tokenization_utils_base import BatchEncoding
 from tqdm import tqdm
@@ -130,11 +130,16 @@ class ContextSelectionExample:
     #: context to assist during prediction
     context: List[str]
     #: 0 => context was not useful, 1 => context was useful
-    label: Literal[0, 1]
+    label: Optional[Literal[0, 1]]
 
 
 class ContextSelectionDataset(Dataset):
-    """"""
+    """
+    :todo: watch out for context direction (does it come from the left
+    or the right ?), it might affect performance and is unhandled as
+    of now.  Maybe we should rethink the input method and enclose
+    context in special tokens such as '[CTX]' ?
+    """
 
     def __init__(self, examples: List[ContextSelectionExample]) -> None:
         self.examples = examples
@@ -159,7 +164,8 @@ class ContextSelectionDataset(Dataset):
             max_length=512,
         )
 
-        batch["label"] = example.label
+        if not example.label is None:
+            batch["label"] = example.label
 
         return batch
 
@@ -167,16 +173,84 @@ class ContextSelectionDataset(Dataset):
 class NeuralContextSelector(ContextSelector):
     """A context selector powered by BERT"""
 
-    def __init__(self, pretrained_model_name: str) -> None:
+    def __init__(
+        self,
+        pretrained_model_name: str,
+        heuristic_retrieval_sents_nb: int,
+        batch_size: int,
+    ) -> None:
+        """
+        :param batch_size: batch size used at inference
+        """
         self.ctx_classifier = BertForSequenceClassification.from_pretrained(
             pretrained_model_name
         )  # type: ignore
+
+        self.heuristic_retrieval_sents_nb = heuristic_retrieval_sents_nb
+        self.same_word_selector = SameWordSelector(heuristic_retrieval_sents_nb)
+
+        self.batch_size = batch_size
 
     def __call__(
         self, sent_idx: int, document: List[NERSentence]
     ) -> Tuple[List[NERSentence], List[NERSentence]]:
         """"""
-        raise NotImplementedError
+        sent = document[sent_idx]
+
+        # get self.heuristic_retrieval_sents_nb potentially important
+        # context sentences
+        left_ctx, right_ctx = self.heuristic_retrieve_ctx(sent_idx, document)
+        ctx_sents = left_ctx + right_ctx
+
+        # prepare datas for inference
+        dataset = ContextSelectionDataset(
+            [
+                ContextSelectionExample(sent.tokens, ctx_sent.tokens, None)
+                for ctx_sent in ctx_sents
+            ]
+        )
+        data_collator = DataCollatorWithPadding(dataset.tokenizer)  # type: ignore
+        dataloader = DataLoader(
+            dataset, batch_size=self.batch_size, shuffle=False, collate_fn=data_collator
+        )
+
+        # inference using self.ctx_classifier
+        self.ctx_classifier = self.ctx_classifier.test()
+        scores = torch.zeros((0,))
+        with torch.no_grad():
+            for X in dataloader:
+                # out.logits is of shape (batch_size, 2)
+                out = self.ctx_classifier(
+                    X["input_ids"],
+                    token_type_ids=X["token_type_ids"],
+                    attention_mask=X["attention_mask"],
+                )
+                scores = torch.cat([scores, out.logits[:, 1].squeeze()], dim=0)
+
+        # now scores should be of shape
+        # (self.heuristic_retrieval_sents_nb). We keep the context
+        # sentence with the max score.
+        best_ctx_idx: int = torch.argmax(scores, dim=0).item()  # type: ignore
+
+        # best_ctx_idx is from left context
+        if best_ctx_idx < len(left_ctx):
+            return ([ctx_sents[best_ctx_idx]], [])
+        # best_ctx_idx is from right context
+        return ([], [ctx_sents[best_ctx_idx]])
+
+    def heuristic_retrieve_ctx(
+        self, sent_idx: int, document: List[NERSentence]
+    ) -> Tuple[List[NERSentence], List[NERSentence]]:
+        """Retrieve potentially useful context sentences to help
+        predict sent at index ``sent_idx``.
+
+        :param sent_idx: index of the sent for which NER predictions
+            are made.
+        :param document: the predicted sentence's document.
+
+        :return: ``(left_context, right_context)``
+        """
+        return self.same_word_selector(sent_idx, document)
 
     @staticmethod
     def generate_context_dataset(
