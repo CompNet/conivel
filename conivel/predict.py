f5dcf1ad835bef4c414c10607eeebf507cb9d13c
Aethor <oow.autre@gmail.com>
2022-07-04 15:44:18 +0200

    WIP: predict enhancements

--- conivel/predict.py
+++ conivel/predict.py
@@ -1,4 +1,5 @@
 from typing import List, Optional, Union, Tuple, cast
+from dataclasses import dataclass, field
 
 import torch
 from torch.utils.data import DataLoader
@@ -12,27 +13,43 @@ from conivel.datas import (
 from conivel.datas.dataset import NERDataset
 
 
+@dataclass
+class PredictionOutput:
+
+    #: a list of tags per sentence
+    tags: List[List[str]] = field(default_factory=lambda: [])
+
+    #: embedding of each tokens (one tensor per sentence). When a token
+    #: is composed of several wordpieces, its embedding is the mean of
+    #: the embedding of the composing wordpieces. Each tensor is of
+    #: shape ``(sentence_size, hidden_size)``.
+    embeddings: List[torch.Tensor] = field(default_factory=lambda: [])
+
+    #: attention
+    attention: List[torch.Tensor] = field(default_factory=lambda: [])
+
+    #: prediction scores, one tensor per sentence. Each tensor is
+    #: of shape ``(sentence_size)``. When a token is composed of
+    #: several wordpieces, its prediction score is the mean of the
+    #: prediction scores.
+    scores: List[torch.Tensor] = field(default_factory=lambda: [])
+
+
 def predict(
     model: BertForTokenClassification,
     dataset: NERDataset,
     batch_size: int = 4,
     quiet: bool = False,
     return_embeddings: bool = False,
-) -> Union[List[List[str]], Tuple[List[List[str]], List[torch.Tensor]]]:
+) -> PredictionOutput:
     """Predict NER labels for a dataset
 
     :param model: the model to use for prediction
     :param dataset: the dataset on which to do predictions
     :param batch_size:
     :param quiet:
-    :param return_embeddings: if ``True``, also returns tokens embeddings.
 
-    :return: a list of tags per sentence. If ``return_embeddings`` is
-        ``True``, additionaly returns the final embeddings of each
-        token in a tuple. Embeddings are returned as a list of torch
-        tensor of shape ``(sentence_size, hidden_size)`` (one per sentence).
-        When a token is composed of several wordpiece, its embedding is the
-        mean of the embedding of the composing wordpieces.
+    :return: a ``PredictionOutput`` object.
     """
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -40,7 +57,7 @@ def predict(
     model = model.to(device)
 
     data_collator = DataCollatorForTokenClassificationWithBatchEncoding(
-        dataset.tokenizer
+        dataset.tokenizer  # type: ignore
     )
     dataloader = DataLoader(
         dataset,
@@ -48,8 +65,7 @@ def predict(
         collate_fn=data_collator,
     )
 
-    sent_tags: List[List[str]] = []
-    sent_embeddings: List[torch.Tensor] = []
+    predict_out = PredictionOutput()
 
     with torch.no_grad():
 
@@ -63,7 +79,8 @@ def predict(
             out = model(
                 input_ids=data["input_ids"],
                 attention_mask=data["attention_mask"],
-                output_hidden_states=return_embeddings,
+                output_hidden_states=True,
+                output_attentions=True,
             )
             assert out.logits.shape == (
                 local_batch_size,
@@ -85,11 +102,12 @@ def predict(
                     prefix_tokens_nb += 1
 
                 tags_nb = len([k for k in data["tokens_labels_mask"][i] if k == 1])  # type: ignore
-                sent_tags.append(["O"] * tags_nb)
+                predict_out.tags.append(["O"] * tags_nb)
 
-                if return_embeddings:
-                    tokens_embeddings = [None] * tags_nb
+                # if return_embeddings
+                tokens_embeddings = [None] * tags_nb
 
+                # for each token
                 for j in range(seq_size):
 
                     t_j = data.token_to_word(i, token_index=j)
@@ -103,36 +121,29 @@ def predict(
                     word_index = t_j - prefix_tokens_nb
                     tag_index = tags_indexs[i][j].item()
 
-                    sent_tags[-1][word_index] = model.config.id2label[tag_index]
+                    predict_out.tags[-1][word_index] = model.config.id2label[tag_index]
 
-                    if return_embeddings:
-                        tokens_embeddings = cast(
-                            List[Optional[torch.Tensor]], tokens_embeddings  # type: ignore
+                    # embeddings
+                    if tokens_embeddings[word_index] is None:
+                        # (hidden_size)
+                        last_hidden_state = out.hidden_states[-1][i, j, :]
+                        tokens_embeddings[word_index] = last_hidden_state
+                        # (1, hidden_size)
+                        tokens_embeddings[word_index].unsqueeze_(0)  # type: ignore
+                    else:
+                        tokens_embeddings[word_index] = torch.cat(
+                            [
+                                # (n, hidden_size)
+                                tokens_embeddings[word_index],
+                                # (1, hidden_size)
+                                out.hidden_states[-1][i, j, :].unsqueeze(0),
+                            ],  # type: ignore
+                            dim=0,
                         )
-                        if tokens_embeddings[word_index] is None:
-                            # (hidden_size)
-                            last_hidden_state = out.hidden_states[-1][i, j, :]
-                            tokens_embeddings[word_index] = last_hidden_state
-                            # (1, hidden_size)
-                            tokens_embeddings[word_index].unsqueeze_(0)
-                        else:
-                            tokens_embeddings[word_index] = torch.cat(
-                                [
-                                    # (n, hidden_size)
-                                    tokens_embeddings[word_index],
-                                    # (1, hidden_size)
-                                    out.hidden_states[-1][i, j, :].unsqueeze(0),
-                                ],  # type: ignore
-                                dim=0,
-                            )
 
-                if return_embeddings:
-                    tokens_embeddings = cast(List[torch.Tensor], tokens_embeddings)  # type: ignore
-                    tokens_embeddings = [
-                        torch.mean(t, dim=0) for t in tokens_embeddings
-                    ]
-                    sent_embeddings.append(torch.stack(tokens_embeddings, dim=0))
+                # add sentence embedding
+                tokens_embeddings = cast(List[torch.Tensor], tokens_embeddings)  # type: ignore
+                tokens_embeddings = [torch.mean(t, dim=0) for t in tokens_embeddings]
+                predict_out.embeddings.append(torch.stack(tokens_embeddings, dim=0))
 
-    if return_embeddings:
-        return (sent_tags, sent_embeddings)
-    return sent_tags
+    return predict_out
