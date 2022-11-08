39407303efbf0f61bd46089d4597663cd5760307
Aethor <oow.autre@gmail.com>
2022-11-08 16:48:47 +0100

    modularize book loading for DekkerDataset

--- conivel/datas/dekker/dekker.py
+++ conivel/datas/dekker/dekker.py
@@ -29,6 +29,55 @@ book_groups = {
 }
 
 
+def load_book(path: str) -> List[NERSentence]:
+
+    # load tokens and tags from CoNLL formatted file
+    tokens = []
+    tags = []
+
+    with open(path) as f:
+
+        for i, line in enumerate(f):
+
+            try:
+                token, tag = line.strip().split(" ")
+            except ValueError:
+                print(f"error processing line {i+1} of book {path}")
+                print(f"line content was : '{line}'")
+                print("trying to proceed...")
+                continue
+
+            tokens.append(token)
+            tags.append(tag)
+
+    # parse into sentences
+    sents = []
+    sent = NERSentence()
+
+    for i, (token, tag) in enumerate(zip(tokens, tags)):
+
+        fixed_token = '"' if token in {"``", "''"} else token
+        fixed_token = "'" if token == "`" else fixed_token
+        next_token = tokens[i + 1] if i < len(tokens) - 1 else None
+
+        sent.tokens.append(fixed_token)
+        sent.tags.append(tag)
+
+        # quote ends next token : skip this token
+        # this avoids problem with cases where we have punctuation
+        # at the end of a quote (otherwise, the end of the quote
+        # would be part of the next sentence)
+        if next_token == "''":
+            continue
+
+        # sentence end
+        if token in ["''", ".", "?", "!"]:
+            sents.append(sent)
+            sent = NERSentence()
+
+    return sents
+
+
 class DekkerDataset(NERDataset):
     """"""
 
@@ -58,50 +107,6 @@ class DekkerDataset(NERDataset):
                 if not name in book_groups[book_group]:
                     continue
 
-            # load tokens and tags from CoNLL formatted file
-            tokens = []
-            tags = []
-
-            with open(book_path) as f:
-
-                for i, line in enumerate(f):
-
-                    try:
-                        token, tag = line.strip().split(" ")
-                    except ValueError:
-                        print(f"error processing line {i+1} of book {book_path}")
-                        print(f"line content was : '{line}'")
-                        print("trying to proceed...")
-                        continue
-
-                    tokens.append(token)
-                    tags.append(tag)
-
-            # parse into sentences
-            doc = []
-            sent = NERSentence()
-
-            for i, (token, tag) in enumerate(zip(tokens, tags)):
-
-                fixed_token = '"' if token in {"``", "''"} else token
-                fixed_token = "'" if token == "`" else fixed_token
-                next_token = tokens[i + 1] if i < len(tokens) - 1 else None
-
-                sent.tokens.append(fixed_token)
-                sent.tags.append(tag)
-
-                # quote ends next token : skip this token
-                # this avoids problem with cases where we have punctuation
-                # at the end of a quote (otherwise, the end of the quote
-                # would be part of the next sentence)
-                if next_token == "''":
-                    continue
-
-                # sentence end
-                if token in ["''", ".", "?", "!"]:
-                    doc.append(sent)
-                    sent = NERSentence()
-
-            documents.append(doc)
+            documents.append(load_book(book_path))
 
         super().__init__(documents, **kwargs)
