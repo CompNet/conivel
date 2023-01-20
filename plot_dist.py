9db6e1d80f5d6598ebe49cdbb5079f458e05ca28
Aethor <oow.autre@gmail.com>
2023-01-20 18:33:40 +0100

    plot adjustments

--- plot_dist.py
+++ plot_dist.py
@@ -25,14 +25,18 @@ bm25_dists = dists["bm25_dists"]
 
 
 plt.style.use("science")
-fig, axs = plt.subplots(1, 2)
-fig.set_size_inches(20, 4)
+plt.rc("xtick", labelsize=30)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=30)  # fontsize of the tick labels
+fig, axs = plt.subplots(1, 2, sharex=True, sharey=True, tight_layout=True)
+fig.set_size_inches(20, 6)
 axs[0].hist(sn_dists, bins=50)
-axs[0].set_xlabel("Distance of retrieved sentences (in sentences)", fontsize=20)
-axs[0].set_title("samenoun", fontsize=20)
+axs[0].set_title("samenoun", fontsize=30)
 axs[1].hist(bm25_dists, bins=50)
-axs[1].set_xlabel("Distance of retrieved sentences (in sentences)", fontsize=20)
-axs[1].set_title("bm25", fontsize=20)
+fig.supxlabel(
+    "Distance of retrieved sentences (in sentences)",
+    fontsize=30,
+)
+axs[1].set_title("bm25", fontsize=30)
 if args.output:
     plt.savefig(args.output)
 else:
--- plot_mean_test_f1.py
+++ plot_mean_test_f1.py
@@ -9,6 +9,25 @@ parser.add_argument("-o", "--output", type=str, default=None)
 parser.add_argument("-r", "--oracle", action="store_true")
 args = parser.parse_args()
 
+# from https://matplotlib.org/stable/gallery/lines_bars_and_markers/linestyles.html
+linestyle_tuple = [
+    ("solid", "solid"),
+    ("dashed", "dashed"),
+    ("dashdot", "dashdot"),
+    ("densely dotted", (0, (1, 1))),
+    ("long dash with offset", (5, (10, 3))),
+    ("densely dashdotdotted", (0, (3, 1, 1, 1, 1, 1))),
+    ("densely dashdotted", (0, (3, 1, 1, 1))),
+    ("dashdotted", (0, (3, 5, 1, 5))),
+    ("densely dashed", (0, (5, 1))),
+    ("loosely dashed", (0, (5, 10))),
+    ("loosely dashdotted", (0, (3, 10, 1, 10))),
+    ("dashdotdotted", (0, (3, 5, 1, 5, 1, 5))),
+    ("loosely dashdotdotted", (0, (3, 10, 1, 10, 1, 10))),
+    ("dotted", "dotted"),
+    ("loosely dotted", (0, (1, 10))),
+]
+
 
 runs = ["random", "bm25", "samenoun", "left", "right", "neighbors"]
 
@@ -17,15 +36,13 @@ with open(f"./runs/bare/metrics.json") as f:
 
 
 plt.style.use("science")
-# plt.rcParams.update({"xtick.labelsize": 18})
-# plt.rcParams.update({"ytick.labelsize": 18})
 plt.rc("xtick", labelsize=40)  # fontsize of the tick labels
 plt.rc("ytick", labelsize=40)  # fontsize of the tick labels
 fig, ax = plt.subplots()
 
-fig.set_size_inches(16, 8)
+fig.set_size_inches(16, 12)
 
-for run in runs:
+for run_i, run in enumerate(runs):
     if args.oracle:
         run = f"oracle_{run}"
     with open(f"./runs/short/{run}/metrics.json") as f:
@@ -33,13 +50,16 @@ for run in runs:
     ax.plot(
         [int(step) for step in metrics["mean_test_f1"]["steps"]],
         metrics["mean_test_f1"]["values"],
+        linestyle=linestyle_tuple[run_i][1],
+        linewidth=4,
     )
 
 # bare baseline
 ax.plot(
     [1, 6],
     [bare_metrics["mean_test_f1"]["values"][0]] * 2,
-    linestyle="--",
+    linestyle=linestyle_tuple[len(runs)][1],
+    linewidth=4,
 )
 
 ax.grid()
