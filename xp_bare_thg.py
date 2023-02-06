251fbe3289475333f3c88b793ceed32933cc870f
Aethor <oow.autre@gmail.com>
2023-02-06 09:53:42 +0100

    Add xp_bare_thg, adjustment for xp_kfolds_thg metrics logging

--- xp_bare_thg.py
+++ xp_bare_thg.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Optional
 import os
 import numpy as np
 from sacred import Experiment
@@ -8,18 +8,11 @@ from sacred.observers import FileStorageObserver, TelegramObserver
 from sacred.utils import apply_backspaces_and_linefeeds
 from conivel.datas.dekker import DekkerDataset
 from conivel.datas.ontonotes import OntonotesDataset
-from conivel.datas.context import context_retriever_name_to_class
 from conivel.datas.the_hunger_games.the_hunger_games import TheHungerGamesDataset
 from conivel.predict import predict
 from conivel.score import score_ner
 from conivel.train import train_ner_model
-from conivel.utils import (
-    RunLogScope,
-    gpu_memory_usage,
-    sacred_archive_huggingface_model,
-    sacred_log_series,
-    pretrained_bert_for_token_classification,
-)
+from conivel.utils import RunLogScope, pretrained_bert_for_token_classification
 
 
 script_dir = os.path.abspath(os.path.dirname(__file__))
@@ -35,7 +28,6 @@ if os.path.isfile(f"{script_dir}/telegram_observer_config.json"):
 
 @ex.config
 def config():
-    # -- datas parameters
     # -- common parameters
     batch_size: int
     # wether models should be saved or not
@@ -43,15 +35,7 @@ def config():
     # number of experiment repeats
     runs_nb: int = 5
 
-    # -- context retrieval
-    # context retriever heuristic name
-    context_retriever: str
-    # context retriever extra args (not including ``sents_nb``)
-    context_retriever_kwargs: dict
-
     # -- NER training parameters
-    # list of number of sents to test
-    sents_nb_list: list
     # number of epochs for NER training
     ner_epochs_nb: int = 2
     # learning rate for NER training
@@ -64,9 +48,6 @@ def main(
     batch_size: int,
     save_models: bool,
     runs_nb: int,
-    context_retriever: str,
-    context_retriever_kwargs: dict,
-    sents_nb_list: List[int],
     ner_epochs_nb: int,
     ner_lr: float,
 ):
@@ -75,9 +56,9 @@ def main(
     dekker = DekkerDataset()
     the_hunger_games = TheHungerGamesDataset()
 
-    precision_matrix = np.zeros((runs_nb, len(sents_nb_list)))
-    recall_matrix = np.zeros((runs_nb, len(sents_nb_list)))
-    f1_matrix = np.zeros((runs_nb, len(sents_nb_list)))
+    precision_matrix = np.zeros((runs_nb,))
+    recall_matrix = np.zeros((runs_nb,))
+    f1_matrix = np.zeros((runs_nb,))
     metrics_matrices = [
         ("precision", precision_matrix),
         ("recall", recall_matrix),
@@ -86,21 +67,15 @@ def main(
 
     for run_i in range(runs_nb):
 
-        ctx_retriever = context_retriever_name_to_class[context_retriever](
-            sents_nb=sents_nb_list, **context_retriever_kwargs
-        )
-        ctx_train_set = ctx_retriever(dekker)
-
-        # train
         with RunLogScope(_run, f"run{run_i}"):
 
             model = pretrained_bert_for_token_classification(
-                "bert-base-cased", ctx_train_set.tag_to_id
+                "bert-base-cased", dekker.tag_to_id
             )
             model = train_ner_model(
                 model,
-                ctx_train_set,
-                ctx_train_set,
+                dekker,
+                dekker,
                 _run=_run,
                 epochs_nb=ner_epochs_nb,
                 batch_size=batch_size,
@@ -110,37 +85,21 @@ def main(
             if save_models:
                 sacred_archive_huggingface_model(_run, model, "model")  # type: ignore
 
-        for sents_nb_i, sents_nb in enumerate(sents_nb_list):
-
-            _run.log_scalar("gpu_usage", gpu_memory_usage())
-
-            ctx_retriever = context_retriever_name_to_class[context_retriever](
-                sents_nb=sents_nb, **context_retriever_kwargs
-            )
-            ctx_test_set = ctx_retriever(the_hunger_games)
-
-            # test
-            test_preds = predict(model, ctx_test_set, batch_size=batch_size).tags
+            preds = predict(model, the_hunger_games, batch_size=batch_size).tags
             precision, recall, f1 = score_ner(
-                ctx_test_set.sents(), test_preds, ignored_classes={"ORG", "LOC"}
+                the_hunger_games.sents(), preds, ignored_classes={"LOC", "ORG"}
             )
-            _run.log_scalar(
-                f"run{run_i}.test_precision",
-                precision,
-                step=sents_nb,
-            )
-            precision_matrix[run_i][sents_nb_i] = precision
-            _run.log_scalar(f"run{run_i}.test_recall", recall, step=sents_nb)
-            recall_matrix[run_i][sents_nb_i] = recall
-            _run.log_scalar(f"run{run_i}.test_f1", f1, step=sents_nb)
-            f1_matrix[run_i][sents_nb_i] = f1
+            _run.log_scalar(f"test_precision", precision)
+            precision_matrix[run_i] = precision
+            _run.log_scalar("test_recall", recall)
+            recall_matrix[run_i] = recall
+            _run.log_scalar("test_f1", f1)
+            f1_matrix[run_i] = f1
 
     # global mean metrics
     for name, matrix in metrics_matrices:
         for op_name, op in [("mean", np.mean), ("stdev", np.std)]:
-            sacred_log_series(
-                _run,
+            _run.log_scalar(
                 f"{op_name}_test_{name}",
-                op(matrix, axis=0),  # (sents_nb)
-                steps=sents_nb_list,
+                op(matrix),
             )
